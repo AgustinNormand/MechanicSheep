77c8a3203466f2c76f446b00c2235fe6ceede8d9
germanfernandez <germanfernandez@users.noreply.github.com>
2020-11-16 11:13:53 -0300

    Arreglo en migracion tabla Tallers

--- database/migrations/2020_11_14_201642_create_tallers_table.php
+++ database/migrations/2020_11_14_201642_create_tallers_table.php
@@ -18,12 +18,12 @@ class CreateTallersTable extends Migration
             $table->string('nombre',200);
             $table->string('descripcion',200);
             $table->string('calle',200);
-            $table->integer('nro_calle',200);
+            $table->integer('nro_calle');
             $table->string('localidad',200);
             $table->string('pais',200);
             $table->string('email',200);
             $table->string('telefono',50);
-            $table->boolean('estado',200);
+            $table->boolean('estado');
             $table->timestamps();
         });
     }
