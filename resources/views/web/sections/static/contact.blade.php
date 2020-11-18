8b4189f9cda3473362bcc17b14f80c1de6243a9b
Melany <melanybulfero97@gmail.com>
2020-11-18 17:57:57 -0300

    Style - contact
    Adding the first styles to contact view

--- resources/views/web/sections/static/contact.blade.php
+++ resources/views/web/sections/static/contact.blade.php
@@ -1,7 +1,34 @@
 @extends('web.layout')
 
-@section('title', 'About')
+@section('title', 'Contact')
+
+@section('style')
+    <link href="{{ asset('css/contact.css') }}" rel="stylesheet">
+@endsection
 
 @section('content')
-    <h1>"Bienvenido a la pagina de Contacto"</h1>
+    <div class="container" id="contact-form">
+        <h1>Contacto</h1>
+        <form>
+            <div class="form-group">
+                <label for="fname">Nombre</label>
+                <input type="text" class="form-control" id="fname" placeholder="Juan">
+            </div>
+            <div class="form-group">
+                <label for="lname">Apellido</label>
+                <input type="text" class="form-control" id="lname" placeholder="Perez">
+            </div>
+            <div class="form-group">
+                <label for="email">Correo Electrónico</label>
+                <input type="email" class="form-control" id="email" placeholder="JuanPerez@example.com">
+            </div>
+            <div class="form-group">
+                <label for="textConsult">Escriba aquí su consulta</label>
+                <textarea class="form-control" id="textConsult" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <a href="#" role="button"> <button type="button" class="btn btn-secondary btn-lg">Enviar</button> </a>
+            </div>
+        </form>
+    </div>
 @endsection()
