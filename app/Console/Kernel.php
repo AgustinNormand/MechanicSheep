9dd727b59c5c5c23d4ac4c75e3fc7ea540f41d4d
Melany <melanybulfero97@gmail.com>
2020-11-21 01:41:38 -0300

    Agrego css contactUs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,36 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            $estimaciones = Estimacion::all();
+            /* Obtengo el día de hoy en formato date*/
+            $today = strtotime('now');
+            $newformatTODAY = date('Y-m-d',$today);
+            foreach ($estimaciones as $estimacion) {
+                $nombre_persona = $estimacion->vehiculo->persona->NOMBRE;
+                /*Transformo el string que obtengo de la db a un formate date*/
+                $time = strtotime($estimacion->FECHA_ESTIMADA_AVISO);
+                $newformat = date('Y-m-d',$time);
+
+                /*Lógica para el envío de emails*/
+                if (($newformat == $newformatTODAY) && ($estimacion->MAIL_ENVIADO == 0)) {
+                    $email_persona = $estimacion->vehiculo->persona->EMAIL;
+                    $correo = new ContactanosMailable($nombre_persona);
+
+                    Mail::to($email_persona)->send($correo);
+
+                    /*Actualizo mail enviado a la bd*/
+                    $estimacion->MAIL_ENVIADO = 1;
+                    $estimacion->save();
+
+                    $correo->build();
+                }
+            }
+        })->everyMinute();
+
     }
 
     /**
