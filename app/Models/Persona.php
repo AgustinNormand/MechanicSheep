e713b4916d8afde4a6992dd39d98880c8dbe7696
Agustin <NormandAgustin@gmail.com>
2020-11-25 12:37:27 -0300

    Agrego funciones para machear un usuario con una persona

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -11,6 +11,16 @@ class Persona extends Model
 
     protected $primaryKey = 'ID_PERSONA';
 
+    protected $guarded = [];
+
+    public static function getByDni($dni){
+        return self::where("NRO_DOC", $dni)->get();
+    }
+
+    public static function getByExactNameAndSurname($nombre, $apellido){
+        return self::where([["NOMBRE", $nombre], ["APELLIDO", $apellido]])->get();
+    }
+
     //Relacion uno a muchos (inversa)
     public function tipo_doc(){
         return $this->belongsTo('App\Models\tipo_doc','ID_TIPO_DOC');
