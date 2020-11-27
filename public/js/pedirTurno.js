69664e205dd9e76bb68c011bec680cb23e25cc2e
Agustin <NormandAgustin@gmail.com>
2020-11-27 12:44:45 -0300

    Arreglo pedirTurno.js

--- public/js/pedirTurno.js
+++ public/js/pedirTurno.js
@@ -1,24 +1,18 @@
-var a = 2; 
-//console.log("The value of a is " + a);
-
 var el = document.querySelector("#service");
-//console.log(el);
-
-//console.log(el.value);
 
 var problema = document.querySelector("#problemaDelAuto");
-console.log(problema);
 
-el.addEventListener('change', (event) => {
-    if(el.value == "Otro servicio mecánico"){
+var textArea = document.querySelector("#problem");
+
+el.addEventListener('change', () => {
+    if(el.value == 13){
         problema.classList.remove('d-none');
-        valor = document.querySelector("#problem");
-        valor.setAttribute("required", "");
-        console.log(el.value);
-    }else{
+        textArea.setAttribute("required", "");
+    }else
+    {
         problema.classList.add('d-none');
-        valor.removeAttribute("required")}
-              
+        textArea.removeAttribute("required")
+    }            
 });
 
 
