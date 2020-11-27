d506986ae656d3d22129ef01873376072f98ba7c
Agustin <NormandAgustin@gmail.com>
2020-11-27 11:31:10 -0300

    Ahora los servicios no estan hardcodeados, sino que los recupera de la db

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -12,21 +12,11 @@ use Illuminate\Support\Facades\Auth;
 class AppointmentController extends Controller
 {
     function get() {
-        $servicios = [
-            "Service - 10.000km",
-            "Service - 20.000km",
-            "Service - 30.000km",
-            "Service - 40.000km",
-            "Service - 50.000km",
-            "Service - 60.000km",
-            "Service - 70.000km",
-            "Service - 80.000km",
-            "Service - 90.000km",
-            "Service - 100.000km",
-            "Service - 110.000km",
-            "Service - 120.000km",
-            "Otro servicio mecánico",
-        ];
+
+        $sector = Sector::where("NOMBRE", "SectorPrincipal")->first();
+
+        $servicios = $sector->servicios;
+
         $vehiculos = Auth::user()->persona->vehiculo;
         return view('web.sections.appointment.appointment', compact('vehiculos', 'servicios'));
     }
