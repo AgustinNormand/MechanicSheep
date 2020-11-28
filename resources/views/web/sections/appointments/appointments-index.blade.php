a999e98ed069afb5e56ee19497056a2212cb7a14
germanfernandez <germanfernandez@users.noreply.github.com>
2020-11-28 11:48:00 -0300

    Update Mis Turnos - Visualizar

--- resources/views/web/sections/appointments/appointments-index.blade.php
+++ resources/views/web/sections/appointments/appointments-index.blade.php
@@ -22,16 +22,100 @@
             <tbody>
                 @foreach ($turnos as $turno)
                     @if ($turno->turno_confirmado)
-                        <tr>
-                            <th scope="row">{{ $turno->turno_confirmado->FECHA_HORA }}</th>
-                            <td>Confirmado</td>
-                            <td>{{ $turno->vehiculo->PATENTE }}</td>
-                            <td>{{ $turno->servicios->first()->NOMBRE }}</td>
-                            <td>
-                                <a href="#" role="button"><button type="button" class="btn btn-secondary btn-sm">Visualizar</button> </a>
-                                <a href="#" role="button"><button type="button" class="btn btn-secondary btn-sm">Cancelar</button> </a>
-                            </td>
-                        </tr>
+                        @if ($turno->turno_confirmado->ESTADO==1)
+                            <tr>
+                                <th scope="row">{{ $turno->turno_confirmado->FECHA_HORA }}</th>
+                                <td>Confirmado</td>
+                                <td>{{ $turno->vehiculo->PATENTE }}</td>
+                                <td>{{ $turno->servicios->first()->NOMBRE }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#exampleModal_{{ ($turno->ID_TURNO_P)}}">Visualizar</button>
+                                    <a href="#" role="button"><button type="button" class="btn btn-secondary btn-sm">Cancelar</button> </a>
+                                </td>
+                            </tr>
+                        @else
+                            <tr>
+                                <th scope="row">{{ $turno->turno_confirmado->FECHA_HORA }}</th>
+                                <td>Cancelado</td>
+                                <td>{{ $turno->vehiculo->PATENTE }}</td>
+                                <td>{{ $turno->servicios->first()->NOMBRE }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#exampleModal_{{ ($turno->ID_TURNO_P)}}">Visualizar</button>
+                                </td>
+                            </tr>
+                        @endif
+
+                        <div class="modal fade" id="exampleModal_{{ ($turno->ID_TURNO_P) }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Detalle de turno</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="fecha" class="col-form-label">Fecha:</label>
+                                                <input type="text" class="form-control" id="fecha" value="{{ $turno->turno_confirmado->FECHA_HORA }}" readonly>
+                                            </div>
+                                        
+                                            <div class="form-group">
+                                                <label for="estado" class="col-form-label">Estado:</label>
+                                                @if ($turno->turno_confirmado->ESTADO == 1)
+                                                    <input type="text" class="form-control" id="estado" value="Confirmado" readonly>
+                                                @else    
+                                                    <input type="text" class="form-control" id="estado" value="Rechazado" readonly>
+                                                @endif
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="modelo" class="col-form-label">Modelo:</label>
+                                                <input type="text" class="form-control" id="modelo" value="{{ $turno->vehiculo->modelo->NOMBRE_FANTASIA }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="marca" class="col-form-label">Marca:</label>
+                                                <input type="text" class="form-control" id="marca" value="{{ $turno->vehiculo->modelo->marca->RAZON_SOCIAL }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="patente" class="col-form-label">Patente:</label>
+                                                <input type="text" class="form-control" id="patente" value="{{ $turno->vehiculo->PATENTE }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="vin" class="col-form-label">VIN:</label>
+                                                <input type="text" class="form-control" id="vin" value="{{ $turno->vehiculo->VIN }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="anio" class="col-form-label">Año:</label>
+                                                <input type="text" class="form-control" id="anio" value="{{ $turno->vehiculo->ANIO }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="numMotor" class="col-form-label">Numero de Motor:</label>
+                                                <input type="text" class="form-control" id="numMotor" value="{{ $turno->vehiculo->NUMERO_MOTOR }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="comentarios" class="col-form-label">Comentarios:</label>
+                                                <textarea class="form-control" id="comentarios" readonly>{{ $turno->COMENTARIOS }}</textarea>
+                                            </div>
+                
+                                        </form>
+                                    </div>               
+            
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                     @else
                         <tr>
                             <th scope="row">{{ $turno->FECHA_SOLICITUD }}</th>
@@ -39,18 +123,92 @@
                             <td>{{ $turno->vehiculo->PATENTE }}</td>
                             <td>{{ $turno->servicios->first()->NOMBRE }}</td>
                             <td>
-                                <a href="#" role="button"><button type="button" class="btn btn-secondary btn-sm">Visualizar</button> </a>
+                            <button type="button" class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#exampleModal_{{ ($turno->ID_TURNO_P) }}">Visualizar</button>
                                 <a href="#" role="button"><button type="button" class="btn btn-secondary btn-sm">Cancelar</button> </a>
                             </td>
                         </tr>
+
+                        <div class="modal fade" id="exampleModal_{{ ($turno->ID_TURNO_P) }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Detalle de turno</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="recipient-name" class="col-form-label">Fecha:</label>
+                                                <input type="text" class="form-control" id="recipient-name" value="{{ $turno->FECHA_SOLICITUD }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="estado" class="col-form-label">Estado:</label>
+                                                <input type="text" class="form-control" id="estado" value="Pendiente" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="modelo" class="col-form-label">Modelo:</label>
+                                                <input type="text" class="form-control" id="modelo" value="{{ $turno->vehiculo->modelo->NOMBRE_FANTASIA }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="marca" class="col-form-label">Marca:</label>
+                                                <input type="text" class="form-control" id="marca" value="{{ $turno->vehiculo->modelo->marca->RAZON_SOCIAL }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="patente" class="col-form-label">Patente:</label>
+                                                <input type="text" class="form-control" id="patente" value="{{ $turno->vehiculo->PATENTE }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="vin" class="col-form-label">VIN:</label>
+                                                <input type="text" class="form-control" id="vin" value="{{ $turno->vehiculo->VIN }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="anio" class="col-form-label">Año:</label>
+                                                <input type="text" class="form-control" id="anio" value="{{ $turno->vehiculo->ANIO }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="numMotor" class="col-form-label">Numero de Motor:</label>
+                                                <input type="text" class="form-control" id="numMotor" value="{{ $turno->vehiculo->NUMERO_MOTOR }}" readonly>
+                                            </div>
+                
+                                            <div class="form-group">
+                                                <label for="comentarios" class="col-form-label">Comentarios:</label>
+                                                <textarea class="form-control" id="comentarios" readonly>{{ $turno->COMENTARIOS }}</textarea>
+                                            </div>
+                
+                                        </form>
+                                    </div>
+                                    
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     @endif
                 @endforeach
             </tbody>
         </table>
     </div>
 
+
 {{--
 Acá hay que tener en cuenta la posibilidad de una paginación (o scroll infinito estaría mejor)
 Y hay que agregar una condición para indicar un aviso que diga "no hay turnos registrados hasta el momento o bien, que muestre la tabla
 --}}
+@endsection
+
+@section('scripts')
+<script>
+    
+</script>   
 @endsection
