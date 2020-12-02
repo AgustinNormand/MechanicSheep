666b0a9f9a85b6a2e1076ac849e7e860b1ff44c4
dbm349 <delmore97@hotmail.com>
2020-12-02 18:46:05 -0300

    Agrego cambios a vista faq.blade y about.blade

--- resources/views/web/sections/static/faq.blade.php
+++ resources/views/web/sections/static/faq.blade.php
@@ -3,5 +3,54 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>"Bienvenido a la pagina de Preguntas Frecuentes"</h1>
+
+    <h1 class="featurette-heading text-center" style="color:#313D57">" PREGUNTAS FRECUENTES "</h1>
+
+    <hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-12">
+        <h2 class="text-center">¿CADA CUÁNTO TIEMPO DEBO REALIZAR EL SERVICIO DE MANTENIMINETO DE MI VEHÍCULO RENAULT?</h2>
+        <p class="lead col-sm-12">- En condiciones normales de uso, el plan de mantenimiento de los modelos de la gama Renault estima realizar los servicios cada 10.000 km o año de uso de la unidad, lo que ocurra primero.</p>
+        <p class="lead col-sm-12">- Su cumplimiento es fundamental para garantizar una correcta vida útil del vehículo, y prevenir incidentes que puedan incluso provocar la inmovilización del vehículo.</p>
+        <p class="lead col-sm-12">- Para más detalles puede dirigirse a la sección "Servicios" que se encuentra en el menú de la página.</p>
+    </div>
+</div>
+
+    <hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-12">
+        <h2 class="text-center">¿CUÁL ES EL COSTO DEL SERVICIO DE MANTENIMIENTO PARA MI VEHÍCULO RENAULT?</h2>
+        <p class="lead col-sm-12">
+        El precio del servicio de mantenimiento dependerá de la versión y el modelo del vehículo,como así también de la zona en la que usted vaya a realizar el mismo. 
+        Para conocer el valor de su servicio de mantenimiento, lo invitamos a visitar http://sites.renault.com.ar/services/services.php. 
+        </p>
+    </div>
+</div>
+
+    <hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-12">
+        <h2 class="text-center">¿CÓMO PUEDO SOLICITAR UN TURNO PARA REALIZAR UN SERVICE A MI VEHÍCULO RENAULT?</h2>
+        <p class="lead col-sm-12">
+        Para ello es necesario que estés registrado en nuestra página, si no lo estas, puedes hacerlo dirígiendote a la sección "Mi cuenta" que se encuentra en el menú de la página.
+Una vez registrado, en la sección de menú "Pedir turno" usted podrá adquirir el turno correspondiente. 
+        </p>
+    </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-12">
+        <h2 class="text-center">¿CADA CUANTO DEBO REALIZAR EL CAMBIO DE BATERIA DE MI VEHÍCULO RENAULT?</h2>
+        <p class="lead col-sm-12">El cambio de bateria se realiza a los 50.000 y 100.000 kms. Para consultar los cambios correspondientes a cada kilometraje que realizamos puede dirigirse a la sección "Servicios" que se encuentra en el menú.
+        </p>
+    </div>
+</div>
+
+
+
 @endsection()
