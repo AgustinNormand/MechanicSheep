063ed05bc75e525035d70972d1d37be200083a46
germanfernandez <germanfernandez@users.noreply.github.com>
2020-12-04 20:57:31 -0300

    Cambio idioma en footer

--- resources/views/web/footer/footer.blade.php
+++ resources/views/web/footer/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="col-12 col-md">
             <img src="https://www.universidadesgratuitas.com/wp-content/uploads/2017/10/logo-universidad-nacional-de-lujan.png" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/>
             <small class="d-block mb-3 text-muted">&copy; 2020</small>
-            <p><a class="text-muted" href="#">Back to top</a></p>
+            <p><a class="text-muted" href="#">Volver a arriba</a></p>
         </div>
         <div class="col-6 col-md">
             <h5>Mundo Renault</h5>
@@ -14,7 +14,7 @@
             </ul>
         </div>
         <div class="col-6 col-md">
-            <h5>About us</h5>
+            <h5>Sobre Nosotros</h5>
             <ul class="list-unstyled text-small">
                 <li>
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
