1855c1f023f814912624f29b3596c6d271ac4cdb
germanfernandez <germanfernandez@users.noreply.github.com>
2020-12-04 21:28:45 -0300

    Update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use App\Models\Estimacion;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Mail\ContactanosMailable;
+use DateTime;
 use Illuminate\Support\Facades\Mail;
 
 class Kernel extends ConsoleKernel
@@ -28,32 +29,29 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            $estimaciones = Estimacion::all();
             /* Obtengo el día de hoy en formato date*/
-            $today = strtotime('now');
-            $newformatTODAY = date('Y-m-d',$today);
-            foreach ($estimaciones as $estimacion) {
+            $today = new DateTime('NOW');
+            
+            $estimaciones = Estimacion::where([
+                ["MAIL_ENVIADO",0],
+                ["FECHA_ESTIMADA_AVISO","<=",$today->format("Y-m-d G:i:s")]
+            ])->get();
+           
+            /*foreach ($estimaciones as $estimacion) {
                 $nombre_persona = $estimacion->vehiculo->persona->NOMBRE;
-                /*Transformo el string que obtengo de la db a un formate date*/
-                $time = strtotime($estimacion->FECHA_ESTIMADA_AVISO);
-                $newformat = date('Y-m-d',$time);
+                
+                $email_persona = $estimacion->vehiculo->persona->EMAIL;
+                $correo = new ContactanosMailable($nombre_persona);
 
-                /*Lógica para el envío de emails*/
-                if (($newformat == $newformatTODAY) && ($estimacion->MAIL_ENVIADO == 0)) {
-                    $email_persona = $estimacion->vehiculo->persona->EMAIL;
-                    $correo = new ContactanosMailable($nombre_persona);
+                Mail::to($email_persona)->send($correo);
 
-                    Mail::to($email_persona)->send($correo);
+                
+                $estimacion->MAIL_ENVIADO = 1;
+                $estimacion->save();
 
-                    /*Actualizo mail enviado a la bd*/
-                    $estimacion->MAIL_ENVIADO = 1;
-                    $estimacion->save();
-
-                    $correo->build();
-                }
-            }
-        })->daily();
-
+                $correo->build(); 
+            }*/
+        })->everyMinute();
     }
 
     /**
