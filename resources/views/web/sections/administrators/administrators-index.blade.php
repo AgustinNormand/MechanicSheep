62bd5638c21fc362dadbe2e3aa7b3b368b878ab8
Melany <melanybulfero97@gmail.com>
2020-12-04 21:21:23 -0300

    Upt administrator view

--- resources/views/web/sections/administrators/administrators-index.blade.php
+++ resources/views/web/sections/administrators/administrators-index.blade.php
@@ -11,6 +11,12 @@
         <h1 id="main-title">Parámetros de configuración</h1>
         <form action="{{route('configurations.store')}}" method="post">
             @csrf
+            <div class="buttons">
+                <input id="send-button" class="btn btn-primary" type="submit" value="Guardar">
+                <button id="upt-button" type="button" class="btn btn-primary">Modificar</button>
+                <button id="cancel-button" type="button" class="btn btn-primary">Cancelar</button>
+            </div>
+            <fieldset disabled>
             <div class="row">
                 <div class="col-sm">
                     @for($i = 0; $i < count($configurations)/2; $i++)
@@ -21,7 +27,6 @@
                             <input name="{{$configurations[$i]->NAME}}" id="{{$configurations[$i]->NAME}}" type="text" class="form-control" value="{{$configurations[$i]->VALUE}}">
                         </div>
                     @endfor
-                    <button id="send-button" type="submit" value="Guardar" class="btn btn-primary">Guardar</button>
                 </div>
                 <div class="col-sm">
                     @for($i = count($configurations)/2; $i < count($configurations); $i++)
@@ -32,10 +37,13 @@
                             <input name="{{$configurations[$i]->NAME}}" id="{{$configurations[$i]->NAME}}" type="text" class="form-control" value="{{$configurations[$i]->VALUE}}">
                         </div>
                     @endfor
-
                 </div>
             </div>
+            </fieldset>
         </form>
     </div>
 @endsection
 
+@section('scripts')
+    <script src="{{ asset('js/administrator.js') }}"></script>
+@endsection
