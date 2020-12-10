775c6aacbd114cc3e6e953541c3a64e745f5f971
Agustin <NormandAgustin@gmail.com>
2020-12-10 11:03:09 -0300

    Agrego los certificados a la configuracion de la db

--- config/database.php
+++ config/database.php
@@ -58,9 +58,13 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'sslmode' => 'require',
+            'options'   => array(
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                PDO::MYSQL_ATTR_SSL_KEY => env('DB_CERTIFICATES_PATH', '').'client-key.pem',
+                PDO::MYSQL_ATTR_SSL_CERT => env('DB_CERTIFICATES_PATH', '').'client-cert.pem',
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_CERTIFICATES_PATH', '').'ca.pem',
+            ),
         ],
 
         'pgsql' => [
